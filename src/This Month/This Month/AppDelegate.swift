0a2502b617e3810aba421162d7a43df4ea7bf5b5
Programus <programus@gmail.com>
2015-02-01 16:23:53 +0800

    Initialized the project.

---
+++ src/This Month/This Month/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  This Month
+//
+//  Created by 王 元 on 15-02-01.
+//  Copyright (c) 2015年 programus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
